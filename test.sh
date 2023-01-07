25c026e91b470adad254b2345a28d2acf19d36a3
Francisco Altimari <francisco@altimari.nl>
2023-01-07 18:54:46 -0500

    Add test animation

---
+++ test.sh
@@ -0,0 +1,17 @@
+{
+  printf '\r[      ]'
+  sleep 1
+  printf '\r[#     ]'
+  sleep 1
+  printf '\r[##    ]'
+  sleep 1
+  printf '\r[###   ]'
+  sleep 1
+  printf '\r[####  ]'
+  sleep 1
+  printf '\r[##### ]'
+  sleep 1
+  printf '\r[######]'
+  sleep 1
+  printf '\n'
+}
